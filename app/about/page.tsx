87adb381c00c9540e4b8373be1b6d9f1ecedaa62
Alisher Iskakov <alisher.iskakov@quanti.cz>
2025-12-09 05:28:47 +0100

    Added about us page

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About us",
+  description: "About us description",
+};
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about"> About us</Link>
+      </p>
     </main>
   );
 }
