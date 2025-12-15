4215f6e4155811d547cae510353f752e616afa41
Alisher4ik <pakapaka1995@mail.ru>
2025-12-15 06:12:48 +0100

    Minor form validation

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,6 +4,10 @@ import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 import { IMeal } from "@/interfaces/meals";
 
+function isInvalidText(text: string) {
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData: FormData) {
   const meal = {
     title: formData.get("title") || "",
@@ -13,6 +17,19 @@ export async function shareMeal(formData: FormData) {
     creator_email: formData.get("email") || "",
     image: formData.get("image"),
   } as IMeal;
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid meal data - please check your input.");
+  }
 
   await saveMeal(meal);
 
